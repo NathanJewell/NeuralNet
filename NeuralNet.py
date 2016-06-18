18ae06888d2b8fd980464901becf2bed7cc27dc0
Zach Kauffman <hyperion9823@gmail.com>
2016-06-18 13:11:40 -0700

    finished feed forward

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,9 +1,12 @@
 
-
+udemi
 
 
 def genWeight():
 	return random()
+
+def hyptan(val):
+	return tanh(val)
 
 class Neuron:
 	'Building block for layers'
@@ -17,8 +20,8 @@ class Neuron:
 		self.biasWeight = ibw
 		self.biasValue = ibv
 
-	weight = 0;			#nueron importance in network
-	deltaWeight = 0;	#difference in weight since last iteration
+	weights = [];			#nueron importance in network
+	deltaWeights = [];	#difference in weight since last iteration
 	value = 0;			#sum of parents
 	desiredValue = 0;	#expected value based on data input
 	error = 0;			#difference between output value and desired
@@ -27,14 +30,18 @@ class Neuron:
 
 class Layer:
 	neurons = []
+	activation = sigmoid()
 
-	def __init__(self, size, bias = True):
+	def __init__(self, size, iactivation):
 		neurons = [Neuron() for i in range(size)]
-		if bias:
-			nuerons.append(Nueron())
 
-	def feedForward(self, parent):
-		print("fed")
+	def feedForward(self, parent, globalBias):
+		sum = 0;
+		for n in self.neurons:
+			for w in range(len(parent.neurons)):
+				sum += parent.neurons[w].value*n.weights[w]
+			n.value = activation(sum + bias)	#is bias part of activation function?
+			sum = 0
 
 	def randomizeWeights(self):
 		for n in neurons:
@@ -42,25 +49,28 @@ class Layer:
 
 class Topology:
 	layers = []		#ordered list of number of neurons in each layer
-	bias = True
+	bias = 0
+	activation = 0
 
-	def setData(self, ilayers, ibias):
+	def setData(self, ilayers, ibias, iactivation):
 		layers = ilayers	
-		bias = ibias		
+		bias = ibias
+		activation = iactivation
 
 class Net:
 	layers = []
+	biasValue = 0;
 
 	def __init__(self, topology):
 		for size in topology.layers:					#create nuerons from topology data 
-			self.layers.append(Layer(size, topology.bias))
+			self.layers.append(Layer(size))
 		for layer in self.layers:
 			layer.randomizeWeights()
 
 	def feedForward(self, input):
 		self.layers[0].setValues(input)
 		for i in range(1, len(layers)-1):	#do calculations for every layer that isn't input
-			self.layers[i].feedForward(layers[i-1])
+			self.layers[i].feedForward(layers[i-1], biasValue)
 
 	def backPropogate(self, desired):
 		print("ya")
@@ -70,12 +80,13 @@ class Net:
 
 def main():
 	top = Topology();
-	top.setData([3, 2, 1], True)
+	top.setData([3, 2, 1], 1, hyptan())
 
 	net = Net(top)
 
-	for i in range(10):
-		print(i)
+	for i in range(iterations):
+		net.feedForward(input)
+		net.backPropogate(desired)
 
 if __name__ == "__main__":
 	main()
